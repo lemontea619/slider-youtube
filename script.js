2978b335a7e1e7866ccd41513f1de7363e7dd39e
lemontea619 <rain_619@icloud.com>
2023-11-06 15:40:12 +0900

    navi button の背景色切り替え処理追加

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+import { translateX, setTranslateX } from "./translateX.js";
 import { clickleftBtm, clickRightBtn } from "./slider-arrow.js";
+
+const listElm = document.querySelector("ul");
+const listItemElm = document.querySelector("li");
 
 const leftBtnElm = document.getElementById("leftBtn");
 leftBtnElm.onclick = clickleftBtm;
@@ -9,7 +13,28 @@ rightBtnElm.onclick = clickRightBtn;
 const btnElms = document.querySelectorAll(".slider-navi-btn");
 btnElms.forEach((elm, i) => {
   elm.addEventListener("click", () => {
-    translateX = -i * listItemElm.scrollWidth;
+    setTranslateX(-i * listItemElm.scrollWidth);
     listElm.style.transform = `translateX(${translateX}px)`;
+    if (i == 0) {
+      btnElms[0].style.backgroundColor = "#000";
+      btnElms[1].style.backgroundColor = "#fff";
+      btnElms[2].style.backgroundColor = "#fff";
+    } else if (i == 1) {
+    } else {
+    }
+    if (i == 1) {
+      btnElms[1].style.backgroundColor = "#000";
+      btnElms[0].style.backgroundColor = "#fff";
+      btnElms[2].style.backgroundColor = "#fff";
+    } else if (i == 1) {
+    } else {
+    }
+    if (i == 2) {
+      btnElms[2].style.backgroundColor = "#000";
+      btnElms[1].style.backgroundColor = "#fff";
+      btnElms[0].style.backgroundColor = "#fff";
+    } else if (i == 1) {
+    } else {
+    }
   });
 });
