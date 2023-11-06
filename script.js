09798defb35f2992827c3405869f127a220d309f
lemontea619 <rain_619@icloud.com>
2023-11-06 16:06:15 +0900

    座標調整

--- script.js
+++ script.js
@@ -11,28 +11,31 @@ const rightBtnElm = document.getElementById("rightBtn");
 rightBtnElm.onclick = clickRightBtn;
 
 const btnElms = document.querySelectorAll(".slider-navi-btn");
+btnElms[0].style.backgroundColor = "#fff";
+btnElms[1].style.backgroundColor = "#000";
+btnElms[2].style.backgroundColor = "#000";
 btnElms.forEach((elm, i) => {
   elm.addEventListener("click", () => {
     setTranslateX(-i * listItemElm.scrollWidth);
     listElm.style.transform = `translateX(${translateX}px)`;
     if (i == 0) {
-      btnElms[0].style.backgroundColor = "#000";
-      btnElms[1].style.backgroundColor = "#fff";
-      btnElms[2].style.backgroundColor = "#fff";
+      btnElms[0].style.backgroundColor = "#fff";
+      btnElms[1].style.backgroundColor = "#000";
+      btnElms[2].style.backgroundColor = "#000";
     } else if (i == 1) {
     } else {
     }
     if (i == 1) {
-      btnElms[1].style.backgroundColor = "#000";
-      btnElms[0].style.backgroundColor = "#fff";
-      btnElms[2].style.backgroundColor = "#fff";
+      btnElms[1].style.backgroundColor = "#fff";
+      btnElms[0].style.backgroundColor = "#000";
+      btnElms[2].style.backgroundColor = "#000";
     } else if (i == 1) {
     } else {
     }
     if (i == 2) {
-      btnElms[2].style.backgroundColor = "#000";
-      btnElms[1].style.backgroundColor = "#fff";
-      btnElms[0].style.backgroundColor = "#fff";
+      btnElms[2].style.backgroundColor = "#fff";
+      btnElms[1].style.backgroundColor = "#000";
+      btnElms[0].style.backgroundColor = "#000";
     } else if (i == 1) {
     } else {
     }
